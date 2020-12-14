59970dd201c2482b23d747eb927f8a8a9ab4a1be
Jose Reyes <jaeyow@gmail.com>
2020-12-14 22:36:39 +1100

    fixed iphone browser header image issue

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,7 +22,7 @@ export const IndexPageTemplate = ({
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
         backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
+        // backgroundAttachment: `fixed`,
       }}
     >
       <div
